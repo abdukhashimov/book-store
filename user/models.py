fd29100b04a725ee7aee6d233044adfd96f23f7e
abdukhashimov <abdukhashimov@yandex.ru>
2020-04-23 16:11:21 +0500

    custom user model

--- user/models.py
+++ user/models.py
@@ -1,7 +1,61 @@
 from django.db import models
 
-# Create your models here.
-# Todo
-# 1. UserManager
-# 2. Base User -- will be the reader
-# 3. Publisher
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, *args, **kwargs):
+        if not email:
+            raise ValueError("User must have an email address!")
+
+        user = self.model(email=self.normalize_email(email), *args, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, *args, **kwargs):
+        user = self.create_user(email, password, *args, **kwargs)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+    def create_publisher(self, email, password=None, *args, **kwargs):
+        user = self.create_user(email, password, *args, **kwargs)
+        user.is_publisher = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_publisher = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    @property
+    def me(self):
+        return self.profile
+
+    def __str__(self):
+        return self.email
+
+
+class UserInfo(models.Model):
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    bio = models.CharField(max_length=255, blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to='profiles/', blank=True, null=True)
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile',
+        blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
